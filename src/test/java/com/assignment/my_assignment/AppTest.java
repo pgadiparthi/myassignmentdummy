3c1237046c869c3eb9f73dd979c4721a66e42a65
Prathap G <prathap@xyz.com>
2024-01-28 14:19:34 -0500

    Commiting my-assignment project

---
+++ src/test/java/com/assignment/my_assignment/AppTest.java
@@ -0,0 +1,22 @@
+package com.assignment.my_assignment;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+    
+   // public void 
+}
